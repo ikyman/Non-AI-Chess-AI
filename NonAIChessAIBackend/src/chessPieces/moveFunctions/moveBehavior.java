050c9551ba82a9bf5b6d949b242291e4a1ad127d
Svet <70716489+ikyman@users.noreply.github.com>
2026-02-06 22:55:41 -0500

    Replaced Move Validity Checker with MoveBehavior.
    What, so we have one object check if the move is valid, and then another to implement the move behavior, ON THE ASSUMPTION that:
    A. we already called "isMoveValid", and
    B. We called the correct "isMoveValid".
    
    No no no no no! One assumption is fine: (No on-the-job-training nor professional experience in the last 2 years because again, no Job. I grant myself the liberty to assume.)
    Two assumptions, well that's just too much. I'm getting Of Water and Wood flashbacks.

--- NonAIChessAIBackend/src/chessPieces/moveFunctions/moveBehavior.java
+++ NonAIChessAIBackend/src/chessPieces/moveFunctions/moveBehavior.java
@@ -2,6 +2,7 @@ package moveFunctions;
 
 import pieceMovement.ChessMove;
 
-public interface moveValidityChecker {
+public interface moveBehavior {
 	public boolean isMoveValid( ChessMove proposedMove);
+	public void moveEffect(ChessMove proposedMove);
 }
