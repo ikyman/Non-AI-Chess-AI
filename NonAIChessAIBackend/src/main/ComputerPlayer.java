5ab730405996da2a2fd37f607be75f8f36f9fd42
Svet <70716489+ikyman@users.noreply.github.com>
2026-02-06 22:27:03 -0500

    Computer Player Now takes in Starting Board
    For use of getting the original piece value, to determine weather the central squares or the squares near the king are considered more stategic.
    
    (The computer player does not yet call the function for summing up a Board's piece value.)

--- NonAIChessAIBackend/src/main/ComputerPlayer.java
+++ NonAIChessAIBackend/src/main/ComputerPlayer.java
@@ -1,14 +1,32 @@
 package main;
 
-import ChessBoard.ChessBoard;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+import checkeredBoard.ChessBoard;
+import checkeredBoard.Coordinate;
+import pieceObjects.ChessPiece;
+import pieceObjects.King;
 
 public class ComputerPlayer {
+	/*static final float MIN_POSITION_VALUE = 1;
+	static final float MAX_POSITION_VALUE = 5;*/
+
 	private ChessColour Colour;
-	public ComputerPlayer(ChessColour Colour) {
+	private int origOpponentPointValue;
+	
+	public ComputerPlayer(ChessColour Colour, ChessBoard starting_board ) {
 		this.Colour = Colour;
 	}
 	
-	public void MakeMove(ChessBoard CurrentBoard){
+	public void MakeMove(ChessBoard current_board){
+		Set<Point> possible_moves = current_board.getMovesForColour(this.Colour);
+		Point EnemyKingLocation = FindEnemyKing(current_board)
 		
+		PriorityQueue<> = new PriorityQueue<>();
+		for (Point pmove : possible_moves ) {
+			
+		}
 	}
+	
 }
