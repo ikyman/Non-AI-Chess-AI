096813fdc1eef94c571fe28c520741c0b47923b9
Svet <70716489+ikyman@users.noreply.github.com>
2026-02-12 12:38:39 -0500

    Revamp Move Generation with new recursion logic.
    See Issue https://github.com/ikyman/Non-AI-Chess-AI/issues/8 :
    This is a step in the right direction, though I see now nothing currently stops the Rook from covering every square on the board.

--- NonAIChessAIBackend/src/chessPieces/pieceMovement/MoveGenerator.java
+++ NonAIChessAIBackend/src/chessPieces/pieceMovement/MoveGenerator.java
@@ -1,52 +1,41 @@
 package pieceMovement;
 
+import java.awt.Point;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
-import ChessBoard.ChessBoard;
-import ChessBoard.Coordinate;
+import checkeredBoard.ChessBoard;
+import checkeredBoard.Coordinate;
 import main.ChessColour;
+import moveFunctions.moveBehavior;
 
 public class MoveGenerator {
-	private Coordinate moveBy;
-	private boolean recursive;
-	private CaptureBehavior captureBehavior;
+	private Set<Point> steps;
+	private moveBehavior mb;
+	private void recurseIf;
 	
-	public MoveGenerator(Coordinate moveBy, boolean recursive, CaptureBehavior captureBehavior ){
-		this.moveBy = moveBy;
-		this.recursive = recursive;
-		this.captureBehavior = captureBehavior;
+	public MoveGenerator(Set<Point> steps, moveBehavior mb,  void recurseIf  ){
+		this.steps = steps;
+		this.mb = mb;
+		this.recurseIf = recurseIf;
 	}
 	
-	public Set<Coordinate> MoveGenerate(Coordinate piece_location, ChessBoard current_board, ChessColour player_colour){
-		HashSet<Coordinate> available_moves= new HashSet<Coordinate>();
+	public Set<ChessMove> MoveGenerate(Point piece_location, ChessBoard current_board, Optional<Point> last_point){
+		HashSet<ChessMove> available_moves= new HashSet<ChessMove>();
 		
-		Coordinate current_location = piece_location;
-		Coordinate next_location = current_location.add(moveBy);
-
-		while (!this.moveBlocked(next_location, current_board, player_colour)){
-			available_moves.add(next_location);
-			if (!recursive) {
-				break;
+		for (Point step: this.steps) {
+			Point new_loc = PointAddition.addPoints( piece_location,  step;);
+			if (last_point.isPresent() && new_loc == last_point.get() ) {
+				continue;
 			}
-			current_location = next_location;
-			next_location = current_location.add(moveBy);
+			ChessMove newMove = new ChessMove(piece_location, new_loc, current_board, this.mb);
+
+			if (recurseIf.canRecurse(newMove) ) {
+				available_moves.addAll( this.MoveGenerate(new_loc, current_board, Optional.of(piece_location) ) );
+			}
+			
 		}
 		return available_moves;
-	}
-	
-	private boolean moveBlocked(Coordinate move, ChessBoard current_board, ChessColour move_colour){
-		if (! current_board.InBounds(move)){
-			return false;
-		} 
-		ChessPiece occupant = current_board.PieceAt(move);
-		if (occupant == null) {
-			return  this.captureBehavior != CaptureBehavior.mandatoryCapture;
-		}
-		if (occupant.getTeamColour() == move_colour){
-				return false;
-		}
-		return this.captureBehavior != CaptureBehavior.cannotCapture;
-		
 	}
 }
