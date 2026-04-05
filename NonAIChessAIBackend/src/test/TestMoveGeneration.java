992efe1c785afbeab66669316bf4f6685326b92c
Svet <70716489+ikyman@users.noreply.github.com>
2026-04-05 13:52:22 -0400

    Add Select "This peice should not move to this point" examples for Non-Recursive Knight.

--- NonAIChessAIBackend/src/test/TestMoveGeneration.java
+++ NonAIChessAIBackend/src/test/TestMoveGeneration.java
@@ -1,6 +1,7 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.awt.Point;
@@ -10,8 +11,11 @@ import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 import checkeredBoard.CheckeredBoard;
+import main.GameColour;
 import pieceMovement.ChessMove;
-import pieceObjects.ChessPiece;
+import pieceMovement.PieceMove;
+import pieceObjects.Knight;
+import pieceObjects.GamePiece;
 
 public class TestMoveGeneration {
 	/* TODO: I foresee it quite likely that I fail to add "This point IS NOT within the possible moves of a piece"
@@ -19,27 +23,30 @@ public class TestMoveGeneration {
 	@Test 
 	void testNoMovesAvailable() {
         CheckeredBoard board = new CheckeredBoard(3);
-        ChessPiece stuckKnight = new Knight();
-        Set<ChessMove> all_moves = stuckKnight.getMoves(new Point(2,2), board);
+        GamePiece stuckKnight = new Knight(GameColour.WHITE);
+        Set<PieceMove> all_moves = stuckKnight.getMoves(new Point(2,2), board);
         assertEquals(all_moves.size(),0);
 	}
 	@Test
 	void testMoves() {
 		CheckeredBoard board = new CheckeredBoard(3);
-        ChessPiece knight_2_moves = new Knight();
-        Set<ChessMove> all_moves = stuckKnight.getMoves(new Point(1,1), board);
+		GamePiece knight_2_moves = new Knight(GameColour.WHITE);
+        Set<PieceMove> all_moves = knight_2_moves.getMoves(new Point(1,1), board);
         assertEquals(all_moves.size(),2);
         Set<Point> move_locations = new HashSet<Point>();
-        for (ChessMove cm: all_moves) {
+        for (PieceMove cm: all_moves) {
         	move_locations.add(cm.getMove_to());
         }
         assertTrue(move_locations.contains(new Point(2,3)));
         assertTrue(move_locations.contains(new Point(3,2)));
+        
+        assertFalse(move_locations.contains(new Point(1,1)));
+        assertFalse(move_locations.contains(new Point(3,3)));
 	}
 	@Test
 	void testRecursiveMoves() {
 		CheckeredBoard board = new CheckeredBoard(5,2);
-        ChessPiece castle = new Rook();
+		GamePiece castle = new Rook();
         Set<ChessMove> all_moves = castle.getMoves(new Point(2,2), board);
         assertEquals(all_moves.size(),5);
         Set<Point> move_locations = new HashSet<Point>();
