a43e96a68861595b4865127c85f783b9042305b0
Svet <70716489+ikyman@users.noreply.github.com>
2026-08-22 13:59:03 -0400

    More Precise name for Priority Quenue computer player.
    "Computer Player" can mean anything! Picking at complete random, a priorityQuenue based solely upon bloodlust, An AI Chess AI, anything!

--- NonAIChessAIBackend/players/main/ComputerPlayerMixedPriorityQuenue.java
+++ NonAIChessAIBackend/players/main/ComputerPlayerMixedPriorityQuenue.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.awt.Point;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.Set;
@@ -9,14 +10,14 @@ import checkeredBoard.CheckeredBoard;
 import checkeredBoard.ChessBoard;
 import pieceMovement.PieceMove;
 
-public class ComputerPlayer implements Player{
+public class ComputerPlayerMixedPriorityQuenue implements Player{
 	static final float MIN_POSITION_VALUE = 1;
 	static final float MAX_POSITION_VALUE = 5;
 
 	private GameColour Colour;
 	private int origOpponentPointValue;
 	
-	public ComputerPlayer(GameColour Colour, CheckeredBoard starting_board ) {
+	public ComputerPlayerMixedPriorityQuenue(GameColour Colour, CheckeredBoard starting_board ) {
 		this.Colour = Colour;
 		this.origOpponentPointValue = this.getTotalEnemyScore(starting_board);	
 	}
@@ -24,7 +25,7 @@ public class ComputerPlayer implements Player{
 	public PieceMove pickMove(CheckeredBoard current_board, Set<PieceMove> RejectedMoves){
 		Set<PieceMove> possible_moves = current_board.getMovesForColour(this.Colour);
 		
-		PriorityQueue<PieceMove> moveRanker = new PriorityQueue<PieceMove>();
+		PriorityQueue<PieceMove> moveRanker = new PriorityQueue<PieceMove>(new AIMoveComparator(current_board));
 		for (PieceMove pmove : possible_moves ) {
 			break;
 		}
@@ -37,7 +38,7 @@ public class ComputerPlayer implements Player{
 		double HalfBoardDiagonal = current_board.DistanceFromCentre(new Point(1,1));
 		double boardDistanceFraction = ( HalfBoardDiagonal- CenterDistance) / (HalfBoardDiagonal);
 		
-		double centerScoreValue = ComputerPlayer.MAX_POSITION_VALUE * boardDistanceFraction + ComputerPlayer.MIN_POSITION_VALUE*(1 - boardDistanceFraction);
+		double centerScoreValue = ComputerPlayerMixedPriorityQuenue.MAX_POSITION_VALUE * boardDistanceFraction + ComputerPlayerMixedPriorityQuenue.MIN_POSITION_VALUE*(1 - boardDistanceFraction);
 		
 		if (current_board instanceof ChessBoard) {
 			double scorePercentRemaining = (this.getTotalEnemyScore(current_board))/this.origOpponentPointValue;
@@ -53,7 +54,7 @@ public class ComputerPlayer implements Player{
 				 kingDistances.add(kingDistanceFraction);
 			}
 			double averageKingFrac = kingDistances.stream().reduce(0.0, (Double a, Double b) -> a+b)/kingDistances.size();
-			double kingScoreValue = ComputerPlayer.MAX_POSITION_VALUE * averageKingFrac + ComputerPlayer.MIN_POSITION_VALUE*(1 - averageKingFrac);
+			double kingScoreValue = ComputerPlayerMixedPriorityQuenue.MAX_POSITION_VALUE * averageKingFrac + ComputerPlayerMixedPriorityQuenue.MIN_POSITION_VALUE*(1 - averageKingFrac);
 			
 			return (centerScoreValue * scorePercentRemaining + kingScoreValue*(1 - scorePercentRemaining) );
 			
@@ -71,4 +72,24 @@ public class ComputerPlayer implements Player{
 		return totalEnemyScore;
 	}
 	
+	final class AIMoveComparator implements Comparator<PieceMove> {
+		private CheckeredBoard board;
+
+		public AIMoveComparator(CheckeredBoard current_board) {
+			this.board = current_board;
+		}
+
+		@Override
+		public int compare(PieceMove o1, PieceMove o2) {
+			double o1PositionScore = getZoneValue(o1.getMove_to(), this.board) - getZoneValue(o1.getMove_from(), this.board);
+			double o2PositionScore = getZoneValue(o2.getMove_to(), this.board) - getZoneValue(o2.getMove_from(), this.board);
+			
+			throw new RuntimeException("Obtaining Kill scores not yet Implemented. ");
+			double o1KillScore = 0;
+			double o2KillScore = 0;
+
+			return (int) ( (o2PositionScore + o2KillScore) - (o1PositionScore + o1KillScore  ));
+		}
+		
+	}
 }
