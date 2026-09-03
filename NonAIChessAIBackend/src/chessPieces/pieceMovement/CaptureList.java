1aa4eb822e0120e4f0c552205b5365771cc4dffe
Svet <70716489+ikyman@users.noreply.github.com>
2026-09-03 17:30:41 -0400

    Meat to the bones of CaptureList
    Improve Commit https://github.com/ikyman/Non-AI-Chess-AI/commit/b7b0a39ddaf138716dd500cd5a1d97718f0a1205 By allowing the CaptureList to be instantiated.
    CaptureList concat is vibe-cded, mostly because the drudgery of writing it myself was too much

--- NonAIChessAIBackend/src/chessPieces/pieceMovement/CaptureList.java
+++ NonAIChessAIBackend/src/chessPieces/pieceMovement/CaptureList.java
@@ -1,10 +1,37 @@
 package pieceMovement;
 
 import java.awt.Point;
+import java.util.HashMap;
 import java.util.Map;
 
 import moveFunctions.capturing.OnCapture;
 
 public class CaptureList {
 	public Map<Point, OnCapture> captures;
+	
+	public static CaptureList EmptyCaptureList() {
+		Map<Point, OnCapture> EmptyMap = new HashMap<Point, OnCapture>();
+		return new CaptureList(EmptyMap);		
+	}
+	
+	CaptureList(Map<Point, OnCapture> captures){
+		this.captures = captures;
+	}
+
+	public CaptureList concat(CaptureList captureList) {
+	    Map<Point, OnCapture> result = new HashMap<Point, OnCapture>(this.captures);
+
+	    for (Map.Entry<Point, OnCapture> entry : captureList.captures.entrySet()) {
+	        if (result.containsKey(entry.getKey())) {
+	            throw new IllegalArgumentException(
+	                "Duplicate Point found while concatenating CaptureLists. " +
+	                "Future improvement: consider storing a List<OnCapture> per Point."
+	            );
+	        }
+
+	        result.put(entry.getKey(), entry.getValue());
+	    }
+
+	    return new CaptureList(result);
+	}
 }
