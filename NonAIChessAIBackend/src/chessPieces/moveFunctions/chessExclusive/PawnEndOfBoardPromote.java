c7ec5ee307494f8223faa10b7de9602c007d3ce8
Svet <70716489+ikyman@users.noreply.github.com>
2026-09-13 16:23:38 -0400

    Create PawnEndOfBoardPromote.java
    Title is enough.
    Two issues: The first is that the pawn is automatically promoted to a Queen, with no choice in the matter. A Queen is more flexible than either a rook or a Bishop, and the situations where a knight is required is sufficiently rare enough that ignoring the possibility can be discarded for now.
    
    The second issue is that any pawn is promoted upon reaching the end of the board. Only the fact that Pawns only move in one direction prevents a white player from moving a pawn one step backwards and getting a free queen.

---
+++ NonAIChessAIBackend/src/chessPieces/moveFunctions/chessExclusive/PawnEndOfBoardPromote.java
@@ -0,0 +1,26 @@
+package moveFunctions.chessExclusive;
+
+import java.awt.Point;
+
+import main.GameColour;
+import moveFunctions.moveBehavior;
+import pieceMovement.PieceMove;
+
+public class PawnEndOfBoardPromote implements moveBehavior{
+
+	@Override
+	public boolean isMoveValid(PieceMove proposedMove) {
+		return true;
+	}
+
+	@Override
+	public void moveEffect(PieceMove proposedMove) {
+		Point promotionSpot = proposedMove.getMove_to();
+		if (promotionSpot.y == 1 || promotionSpot.y == proposedMove.getCurrent_board().get_y_dim()) {
+			GameColour promotion_team = proposedMove.getCurrent_board().getPieceAt(promotionSpot).getTeamColour();
+			proposedMove.getCurrent_board().clearPoint(promotionSpot);
+			Queen promotedQueen = new Queen(promotion_team);
+			proposedMove.getCurrent_board().placePiece(promotedQueen, promotionSpot);
+		}
+	}
+}
