5a4661768acc46699be10c057a80de47afc0c578
Svet <70716489+ikyman@users.noreply.github.com>
2026-09-21 00:27:32 -0400

    Band-aid solution to Unchecked Exception in a Bad function.
    See AI Prompt. I don't like moveFitsGameRules. Therefore, the fact that I surround makeMove with the autogenerated catch block (generally considered bad practice) is irrelavent.
    It'll be deleted soon enough.

--- NonAIChessAIBackend/src/checkeredBoard/ChessBoard.java
+++ NonAIChessAIBackend/src/checkeredBoard/ChessBoard.java
@@ -13,7 +13,7 @@ public class ChessBoard extends CheckeredBoard {
 		for (int x = 1; x < this.x_size; x++) {
 			for (int y = 1; y < this.y_size; y++) {
 				Point coord = new Point(x,y);
-				GamePiece piece_at_coord = this.PieceAt(coord);
+				GamePiece piece_at_coord = this.getPieceAt(coord);
 				if ((piece_at_coord instanceof King )&& piece_at_coord.getTeamColour() == kingColour) {
 					return coord;
 				}
@@ -24,16 +24,37 @@ public class ChessBoard extends CheckeredBoard {
 	
 	@Override
 	public boolean moveFitsGameRules(PieceMove proposed_move) {
+		// Thanks, I hate this. Why not simply Write rulesets as functions, and then add them to the Checkered board?
 		// Save the current state of the board. AI written
-		ChessBoard testingBoard = this.clone();
 		
+		/* AI Prompt for when I once again have AI Juice:
+		 * Why are we Having the Chessboard govern anything? We already have an interface for determining if a move is valid:
+		 * the moveBehavior interface.
+		 * 
+		 * Take this Chessboard moveFitsGameRules. This checks that no MOVE puts a king in check. AKA, it checks move validity.
+		 * Why not instead add that requirement upon generating moves? PeiceMove has a CompositeMoveBehaviour, and when generating moves, add
+		 * the "cannot put king in check" moveBehaviour to any generated PieceMove
+		 * */ 
+		PieceMove testMove = proposed_move.clone();
+	
         // Get the team whose move is being tested
-		GameColour teamColour = proposed_move.getTeamColour();
-		testingBoard.makeMove(proposed_move);
-		Point kingLocation = testingBoard.getKingLocation(teamColour);
+		GameColour teamColour = proposed_move.getCurrent_board().getPieceAt(proposed_move.getMove_from()).getTeamColour();
+		try {
+			testMove.makeMove();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Point kingLocation = ((ChessBoard) testMove.getCurrent_board()).getKingLocation(teamColour);
 
         // If the king is in the danger zone, the move is invalid
-        Set<Point> danger = testingBoard.dangerZone(teamColour);
+        Set<Point> danger = testMove.getCurrent_board().dangerZone(teamColour);
+                
         return !danger.contains(kingLocation);
+	}
+	
+	@Override
+	public ChessBoard clone() {
+		return (ChessBoard) super.clone();
 	}
 }
